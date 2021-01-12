8122321a77adf3dcf61598ad3fc315e8ee25721c
aureliennezzar <aurelien.nezzar@outlook.fr>
2021-01-12 20:57:50 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+const { default: Map } = require("domain/Map/Map");
+const { default: MapContainer } = require("domain/MapContainer/MapContainer");
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
